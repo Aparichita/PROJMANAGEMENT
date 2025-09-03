ce70ee7e01f50a96c0619de9e843769c7812dc5b
aparichita <shreyohh@gmail.com>
2025-09-04 00:49:11 +0530

    Update app.js: integrate auth routes and configure middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import healthCheckRouter from "./routes/healthcheck.route.js";
+import authRouter from "./routes/auth.routes.js";
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
@@ -17,6 +18,7 @@ app.use(
 );
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to basecampy");
