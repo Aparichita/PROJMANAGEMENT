915af0e97e796f273409159706e2606b922c99c2
aparichita <shreyohh@gmail.com>
2025-09-12 23:35:56 +0530

    Updated cookie Parser

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import healthCheckRouter from "./routes/healthcheck.route.js";
 import authRouter from "./routes/auth.routes.js";
-
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 app.use(
   cors({
@@ -25,6 +26,5 @@ app.use("/api/v1/auth", authRouter);
 app.get("/", (req, res) => {
   res.send("Welcome to basecampy");
 });
-
 
 export default app;
